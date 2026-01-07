140a75140296d0e61c2753845728dfbf7e6edc62
Sahil dev barman <sahildevbarman369@gmail.com>
2026-01-07 19:56:37 +0530

    new file

---
+++ 32_exception_handling/O1.java
@@ -0,0 +1,43 @@
+class O1 {
+    public static void main(String[] args) {
+        System.out.println("~~~~~~~~~~~~ 1");
+
+        try {
+            int i = Integer.parseInt(args[0]);
+            System.out.println("~~~~~~~~~~~~ 2");
+
+            int[] x = {3, 0, 2};
+            int n = x[i];
+            System.out.println("~~~~~~~~~~~~~ 3");
+            System.out.println(n);
+
+            int z = 12/n;
+            System.out.println("~~~~~~~~~~~~~~ 4");
+            System.out.println(z);
+
+            System.out.println("~~~~~~~~~~~~ Essential Code ~~~~~~~~~~");
+        }
+        catch(ArrayIndexOutOfBoundsException e) {
+            System.out.println(e + " AIOBE");
+            System.out.println("~~~~~~~~~ Essential Code ~~~~~~~~~~~~");
+        }
+        catch(NumberFormatException e) {
+            System.out.println(e + " NFE");
+            System.out.println("~~~~~~~~~ Essential Code ~~~~~~~~~~~~");
+        }
+        catch(ArithmeticException e) {
+            System.out.println(e + " AE");
+            System.out.println("~~~~~~~~~ Essential Code ~~~~~~~~~~~~");
+        }
+        System.out.println("~~~~~~~~~~~~~~~~ 5");
+    }
+}
+
+
+// commandline argument
+// input: no input
+// input: a
+// input: 1
+// input: 2
+// input: 0
+// input: 3
