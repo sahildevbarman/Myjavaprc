8dd8f9e399eb47f06a918da57941a0e915870135
Sahil dev barman <sahildevbarman369@gmail.com>
2026-02-03 17:34:34 +0530

    2026-02-03 | Exception Handling - new example

---
+++ 33_exception_handling/Q.java
@@ -0,0 +1,8 @@
+import java.io.FileNotFoundException;
+class Q {
+    public static void main(String[] args) throws FileNotFoundException {
+        System.out.println(1);  
+                    
+        System.out.println(2);              
+    }
+}
