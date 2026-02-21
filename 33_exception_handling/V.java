9bf23ead2f9b4683d602f0398e5f2d170adcc29b
Sahil dev barman <sahildevbarman369@gmail.com>
2026-02-21 20:59:29 +0530

    2026-02-21 | Exception Handling - new example

---
+++ 33_exception_handling/V.java
@@ -0,0 +1,12 @@
+class V {
+    public static void main(String[] args) {
+        try {
+
+        // } catch(ArithmeticException e) {
+        // } catch(RuntimeException e) {
+        // } catch(Error e) {
+        } catch(IllegalArgumentException e) {
+
+        }
+    }    
+}
