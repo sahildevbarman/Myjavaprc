65af2aea06ab458c0d1a2bad4535f998ffb5d311
Sahil dev barman <sahildevbarman369@gmail.com>
2026-02-24 23:18:28 +0530

    2026-02-24 | Exception Handling - new example

---
+++ 33_exception_handling/V3.java
@@ -0,0 +1,17 @@
+class V3 {
+    public static void main(String[] args) {
+        
+        try {
+
+        } catch(Object e) {
+
+        }
+        
+    }    
+}
+
+
+// V3.java:6: error: incompatible types: Object cannot be converted to Throwable
+//         } catch(Object e) {
+//                 ^
+// 1 error
