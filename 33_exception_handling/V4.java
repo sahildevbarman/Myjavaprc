b24df3efe69b772aadc3e29979de05dd64178377
Sahil dev barman <sahildevbarman369@gmail.com>
2026-02-25 23:08:23 +0530

    2026-02-25 | Exception Handling - new example

---
+++ 33_exception_handling/V4.java
@@ -0,0 +1,15 @@
+class V4 {
+    public static void main(String[] args) {
+        try {
+
+        } catch(V4 x) {
+
+        }
+    }
+}
+
+
+// V4.java:5: error: incompatible types: V4 cannot be converted to Throwable
+//         } catch(V4 x) {
+//                 ^
+// 1 error
