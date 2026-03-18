8eb1233f5e49de10af649254074b70fe40bb8f76
Sahil dev barman <sahildevbarman369@gmail.com>
2026-03-18 21:15:23 +0530

    2026-03-18 | Exception Handling - new example

---
+++ 34_exception_handling/N.java
@@ -0,0 +1,14 @@
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+class N {
+    public static void pro() throws IOException {
+        System.out.println("Hi");
+    } 
+
+    private int pro(int x) throws FileNotFoundException {
+        System.out.println("Hi");
+
+        return 2;
+    }    
+}
