ed6b26c4284297fff95cc68e914e2305eb901ca9
Sahil dev barman <sahildevbarman369@gmail.com>
2026-03-19 21:40:25 +0530

    2026-03-19 | Exception Handling - new example

---
+++ 34_exception_handling/O.java
@@ -0,0 +1,14 @@
+import java.io.FileNotFoundException;
+import java.io.EOFException;
+
+class N {
+    public static void pro() throws FileNotFoundException {
+        System.out.println("Hi");
+    } 
+
+    private int pro(int x) throws EOFException {
+        System.out.println("Hi");
+
+        return 2;
+    }
+}
