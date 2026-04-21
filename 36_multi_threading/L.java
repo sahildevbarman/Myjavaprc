c4971fc6cd4e28b01ab38bb018c79d450f732fc2
Sahil dev barman <sahildevbarman369@gmail.com>
2026-04-21 21:45:50 +0530

    2026-04-21 | Multi Threading - new example

---
+++ 36_multi_threading/L.java
@@ -0,0 +1,38 @@
+class L {
+    static AA x = new AA();
+
+    public static void main(String[] args) {
+        Thread a = new Thread(new C1Runnable(), "A"); 
+        Thread b = new Thread(new C2Runnable(), "B");
+        
+        a.start();
+        b.start();
+    }
+}
+
+class AA {
+    synchronized void aaa() {
+        Thread t = Thread.currentThread();
+        for(int i=0;i<30;i++) {
+            System.out.println(t.getName() + " - " + i);
+        }
+    }
+
+    static synchronized void bbb() {
+        Thread t = Thread.currentThread();
+        for(int i=0;i<30;i++) {
+            System.out.println(t.getName() + " - " + i);
+        }
+    }
+}
+
+class C1Runnable implements Runnable {
+    public void run() {
+        L.x.aaa();
+    }
+}
+class C2Runnable implements Runnable {
+    public void run() {
+        L.x.bbb();
+    }
+}
