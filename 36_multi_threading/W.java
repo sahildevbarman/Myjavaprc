fa6137e789eb0e5bce8aa522d19d8de3f4471901
Sahil dev barman <sahildevbarman369@gmail.com>
2026-05-01 20:21:30 +0530

    2026-04-01 | Multi Threading - new example

---
+++ 36_multi_threading/W.java
@@ -0,0 +1,30 @@
+public class DThread extends Thread {
+    public void run() {
+        EThread t = new EThread();
+        t.setName("Bholu");
+        t.start();
+    }
+}
+
+class EThread extends Thread {
+    public void run() {
+        Thread t = Thread.currentThread();       
+        System.out.println(t.getName() + " - " + t.isDaemon());        
+    }
+}
+
+class W {
+    public static void main(String[] args) {
+        DThread x = new DThread();
+        x.setName("Golu");
+        x.setDaemon(true);
+        x.start();
+        
+        Thread t = Thread.currentThread();
+        for(int i=0;i<30;i++) {
+            System.out.println(t.getName() + " - " + i);
+        }
+    }
+} {
+    
+}
