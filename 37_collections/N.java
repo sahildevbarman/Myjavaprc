1b7dfe82d4179a7fcc426bf59e91f487fc76e477
Sahil dev barman <sahildevbarman369@gmail.com>
2026-05-11 23:12:37 +0530

    2026-05-11 | Collection - new example

---
+++ 37_collections/N.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+
+class N {
+    public static void main(String[] args) {
+        ArrayList x = new ArrayList();
+
+        
+        x.add("mohan");
+        x.add(2.34);
+        x.add(74);
+        x.add(true);
+        x.add('Y');
+
+        System.out.println(x);
+
+        x.get(0).length();
+        x.get(0).toString();
+    }
+}
