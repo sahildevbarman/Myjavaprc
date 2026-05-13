b73ce8189edb15b961c6186e901affcb19aa758f
Sahil dev barman <sahildevbarman369@gmail.com>
2026-05-13 14:34:06 +0530

    2026-05-13 | Collection - new example

---
+++ 37_collections/O.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+
+class O {
+    public static void main(String[] args) {
+        ArrayList x = new ArrayList();
+
+        // x.add(34);
+
+        System.out.println(x);
+    }
+}
