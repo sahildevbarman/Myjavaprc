c2f9b9efca500c3ebe87bbf43fe66807f2851260
Sahil dev barman <sahildevbarman369@gmail.com>
2026-05-14 14:08:52 +0530

    2026-05-14 | Collection - new example

---
+++ 37_collections/P.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+
+class P {
+    public static void main(String[] args) {
+        // ArrayList x = new ArrayList();
+        ArrayList<Integer> x = new ArrayList<Integer>();
+
+        x.add(34);
+
+        System.out.println(x);
+    }
+}
