fae67965c580f85f63d69a1073c3dcf1313434d4
Sahil dev barman <sahildevbarman369@gmail.com>
2026-05-16 22:48:01 +0530

    2026-05-16 | Collection - new example

---
+++ 37_collections/R.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+
+class R {
+    public static void main(String[] args) {
+        ArrayList<String> x = new ArrayList<String>();
+
+        x.add("mohan");
+        x.add("sohan");
+        x.add("rohan");
+        x.add("gohan");
+        x.add("tohan");
+
+        x.get(3).length();   //Also check N and N1.java
+    }
+}
