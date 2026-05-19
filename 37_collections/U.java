baec7d79221d477f756e2907688b3a10fb56f02d
Sahil dev barman <sahildevbarman369@gmail.com>
2026-05-19 18:49:15 +0530

    2026-05-19 | Collection - new example

---
+++ 37_collections/U.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+
+class U {
+    public static void main(String[] args) {
+        ArrayList x = new ArrayList();
+
+        x.add(new Student("mohan", 12));
+        x.add(new Student("sohan", 10));
+        x.add(new Student("rohan", 8));
+        x.add(new Student("gohan", 15));
+        x.add(new Student("tohan", 13));
+
+        System.out.println(x);
+
+        Student a = new Student("rohan", 8);
+        x.remove(a);
+
+        System.out.println(x);
+    }
+}
+
+class Student {
+    String name;
+    int age;
+
+    Student(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String toString() {
+        return name + " - " + age;
+    }
+}
