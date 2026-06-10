40cdb329f31d284b38e1eb8bf52c63baa408f0fc
Sahil dev barman <sahildevbarman369@gmail.com>
2026-06-10 20:54:59 +0530

    2026-06-10 | Collection - new example

---
+++ 38_collections/J.java
@@ -0,0 +1,11 @@
+import java.util.HashSet;
+
+class J {
+    public static void main(String[] args) {
+        HashSet set = new HashSet();
+
+        System.out.println(set.size());
+        System.out.println(set.isEmpty());
+        System.out.println(set);
+    }    
+}
