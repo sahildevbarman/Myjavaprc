7fb0a235c26d0bbae97cc5d96b670d674790a43d
Sahil dev barman <sahildevbarman369@gmail.com>
2026-06-12 22:22:37 +0530

    2026-06-12 | Collection - new example

--- 38_collections/M.java
+++ 38_collections/M.java
@@ -1,19 +1,16 @@
 import java.util.HashSet;
 
-class L {
+class M {
     public static void main(String[] args) {
         HashSet set = new HashSet();
-
-        System.out.println(set);
 
         set.add(56);
         set.add(12);
         set.add(68);
-        set.add(56);
         set.add(21);
         set.add(19);
-        set.add(56);
-
-        System.out.println(set);
-    }
-} 
+        
+        // System.out.println(set.contains(78));
+        System.out.println(set.contains(68));
+    }    
+}
