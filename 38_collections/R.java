e52e642f00bd1a6e32c43e9149498dbaf4d51009
Sahil dev barman <sahildevbarman369@gmail.com>
2026-06-20 20:41:27 +0530

    2026-06-20 | Collection - new example

---
+++ 38_collections/R.java
@@ -0,0 +1,15 @@
+import java.util.HashSet;
+
+class R {
+    public static void main(String[] args) {
+        HashSet set = new HashSet();
+
+        set.add("mohan");
+        set.add(12);
+        set.add(true);
+        set.add(4.56);
+
+        System.out.println(set);
+    }
+    
+}
