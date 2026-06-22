f6188d1024d0fae8ecb4d56100c136c5dc25aab4
Sahil dev barman <sahildevbarman369@gmail.com>
2026-06-22 22:35:37 +0530

    2026-06-22 | Collection - new example

---
+++ 38_collections/S1.java
@@ -0,0 +1,30 @@
+import java.util.HashSet;
+
+class S1 {
+    public static void main(String[] args) {
+        HashSet set = new HashSet();
+
+        set.add(new Student("hritik", 12));
+        set.add(new Student("ratnesh", 18));
+        set.add(new Student("raj", 9));
+        set.add(new Student("vikki", 12));
+        set.add(new Student("lukky", 15));
+        set.add(new Student("raj", 9));
+
+        System.out.println(set);
+    } 
+}
+
+class Student {
+    String name;
+    int age;
+
+    Student(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String toString() {
+        return name + " - " + age;
+    }
+}
