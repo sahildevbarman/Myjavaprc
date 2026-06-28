0b11e48c8d14fa2a9b08a2a73c066b53a392fb87
Sahil dev barman <sahildevbarman369@gmail.com>
2026-06-28 10:01:42 +0530

    2026-06-28 | Collection - new example

---
+++ 38_collections/X.java
@@ -0,0 +1,47 @@
+class X {
+    public static void main(String[] args) {
+        Student x = new Student("Gurmeet", 21);
+        Teacher y = new Teacher("Gurmeet", 21);
+
+        System.out.println(x.equals(y));
+    }
+}
+
+class Teacher {
+    String name;
+    int age;
+
+    Teacher(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String toString() {
+        return name + " ~ " + age;
+    } 
+}
+
+
+class Student {
+    String name;
+    int age;
+
+    Student(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String toString() {
+        return name + " ~ " + age;
+    } 
+
+    public boolean equals(Object obj) {
+        Student a = this;
+        Student b = (Student)obj;
+
+        String nm1 = a.name;
+        String nm2 = b.name;
+
+        return nm1.equals(nm2);
+    }
+}
