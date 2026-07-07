656d6bcfdb98e6608e3fbfa6c2d0e5d49e5f580c
Sahil dev barman <sahildevbarman369@gmail.com>
2026-07-07 21:01:40 +0530

    2026-07-07 | Collection - new example

---
+++ 39_collections/D1.java
@@ -0,0 +1,15 @@
+import java.util.TreeSet;
+
+class D1 {
+    public static void main(String[] args) {
+        TreeSet set = new TreeSet();
+
+        set.add("jay");
+        set.add("mohan");
+        set.add("bablu");
+        set.add("rohan");
+        set.add("ganesh");
+
+        System.out.println(set);
+    }
+}
