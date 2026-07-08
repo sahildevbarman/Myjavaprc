db2552088178766a689c1cf12b70ffb26f69537f
Sahil dev barman <sahildevbarman369@gmail.com>
2026-07-08 21:15:08 +0530

    2026-07-08 | Collection - new example

---
+++ 39_collections/D2.java
@@ -0,0 +1,15 @@
+import java.util.TreeSet;
+
+class D2 {
+    public static void main(String[] args) {
+        TreeSet set = new TreeSet();
+
+        set.add(" a");
+        set.add("1j");
+        set.add(" M");
+        set.add("U2");
+        set.add("5H");
+
+        System.out.println(set);
+    }
+}
