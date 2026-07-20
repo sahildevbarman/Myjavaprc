3f917f409cbd9a0b67aac2901818319b1208dc21
Sahil dev barman <sahildevbarman369@gmail.com>
2026-07-20 13:36:30 +0530

    2026-07-20 | Collection - new example

---
+++ 39_collections/J1.java
@@ -0,0 +1,7 @@
+class J1 {
+    public static void main(String[] args) {
+        String x = new String("vikram");
+        String y = new String("vikrom");
+        System.out.println(y.compareTo(x));
+    }
+}
