1da051beea5ccf8798d516ded41252ceba4c223c
Sahil dev barman <sahildevbarman369@gmail.com>
2026-07-21 22:16:54 +0530

    2026-07-21 | Collection - new example

---
+++ 39_collections/K.java
@@ -0,0 +1,40 @@
+import java.util.TreeSet;
+
+class K {
+    public static void main(String[] args) {
+        TreeSet set = new TreeSet();
+
+        set.add(new Student("karthik", 18));
+        set.add(new Student("simha", 9));
+        set.add(new Student("vijendra", 12));
+        set.add(new Student("prasad", 5));
+        set.add(new Student("ganesh", 10));
+
+        System.out.println(set);
+    }
+}
+
+
+class Student implements Comparable {
+    String name;
+    Integer age;
+
+    Student(String name, Integer age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String toString() {
+        return name + " ^ " + age;
+    }
+
+    public int compareTo(Object obj) {
+        Student x = this;
+        Student y = (Student)obj;
+
+        String nm1 = x.name;
+        String nm2 = y.name;
+
+        return nm1.compareTo(nm2);
+    }
+}
