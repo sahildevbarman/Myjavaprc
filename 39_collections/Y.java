03d1a857538fa1872cde40a135aa37ee0102bd0d
Sahil dev barman <sahildevbarman369@gmail.com>
2026-08-06 12:54:37 +0530

    2026-08-06 | Collection - new example

---
+++ 39_collections/Y.java
@@ -0,0 +1,17 @@
+import java.util.HashMap;
+
+class Y {
+    public static void main(String[] args) {
+        HashMap map = new HashMap();
+
+        map.put("mohan", 78);
+        map.put("rohan", 89);
+        map.put("sohan", 62);
+        map.put("ganesh", 100);
+        map.put("vikram", 56);
+
+        System.out.println(map);
+        System.out.println(map.size());
+        System.out.println(map.isEmpty());
+    }
+}
