322ae73fe885d363fef8ebea5d36f34af91c84cd
Sahil dev barman <sahildevbarman369@gmail.com>
2026-08-12 21:22:30 +0530

    2026-08-12 | Collection - new example

---
+++ 40_collections/C1.java
@@ -0,0 +1,16 @@
+import java.util.HashMap;
+
+class C1 {
+    public static void main(String[] args) {
+        HashMap map = new HashMap();
+
+        map.put("mohan", 78);
+        map.put("rohan", 89);
+        map.put("sohan", 62);
+        map.put("ganesh", 100);
+        map.put("vikram", 56);
+
+        Integer x = (Integer)map.get("sohan");
+        System.out.println(x);
+    }
+}
