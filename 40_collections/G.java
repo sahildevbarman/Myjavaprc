360e89764c96d6487a3f25def59623018c7422d6
Sahil dev barman <sahildevbarman369@gmail.com>
2026-08-19 19:39:16 +0530

    2026-08-19 | Collection - new example

---
+++ 40_collections/G.java
@@ -0,0 +1,22 @@
+import java.util.HashMap;
+import java.util.Set;
+
+class G {
+    public static void main(String[] args) {
+        HashMap<String,Integer> map = new HashMap<String,Integer>();
+
+        map.put("mohan", 78);
+        map.put("rohan", 89);
+        map.put("sohan", 62);
+        map.put("ganesh", 100);
+        map.put("vikram", 56);
+
+        System.out.println(map);
+
+        Set<String> set = map.keySet();
+
+        for(String key : set) {
+            System.out.println(key);
+        }
+    }
+}
