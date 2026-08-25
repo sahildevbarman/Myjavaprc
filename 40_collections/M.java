92bbb83954cf0c67e9f2a703b10d779a142ba9b6
Sahil dev barman <sahildevbarman369@gmail.com>
2026-08-25 17:46:41 +0530

    2026-08-25 | Collection - new example

---
+++ 40_collections/M.java
@@ -0,0 +1,20 @@
+import java.util.Hashtable;
+
+class M {
+    public static void main(String[] args) {
+        Hashtable<String,Integer> map = new Hashtable<String,Integer>();
+
+        map.put("mohan", 78);
+        map.put("rohan", null);
+        map.put("sohan", 62);
+        map.put("ganesh", 100);
+        map.put("vikram", null);        
+
+        System.out.println(map);
+    }
+}
+
+
+// Exception in thread "main" java.lang.NullPointerException
+//         at java.base/java.util.Hashtable.put(Hashtable.java:480)
+//         at N.main(N.java:8)
