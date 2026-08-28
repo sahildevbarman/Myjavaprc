4ee2a380d8b5a86793286343f66c94e68a42869a
Sahil dev barman <sahildevbarman369@gmail.com>
2026-08-28 18:04:20 +0530

    2026-08-28 | Collection - new example

---
+++ 40_collections/P.java
@@ -0,0 +1,16 @@
+import java.util.TreeMap;
+
+class P {
+    public static void main(String[] args) {
+        TreeMap map = new TreeMap();
+
+        map.put(789, "mohan");
+        map.put(234, "hitesh");
+        map.put(621, "raghav");
+        map.put(128, "naman");
+        map.put(834, "sudhir");
+        map.put(542, "jagtap");
+
+        System.out.println(map);
+    }
+}
