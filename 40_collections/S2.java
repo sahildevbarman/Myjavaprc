4deff65eac7876cb657ace619bce18972a31b7b4
Sahil dev barman <sahildevbarman369@gmail.com>
2026-09-04 19:47:45 +0530

    2026-09-04 | Collection - new example

---
+++ 40_collections/S2.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+
+class S2 {
+    public static void main(String[] args) {
+        ArrayList<Integer> x = new ArrayList<Integer>();
+
+        x.add(12);
+        x.add(13);
+        x.add(15);
+        x.add(21);
+        x.add(9);
+
+        Object[] arr = x.toArray();
+
+        for(Object obj : arr) {
+            System.out.println(obj);
+        }
+
+        System.out.println(arr[0]);
+        System.out.println(arr[1]);
+        System.out.println(arr[2]);
+        System.out.println(((Integer)arr[3]).intValue());
+        System.out.println(arr[4]);
+
+        // System.out.println(arr.get(0));
+    }
+}
