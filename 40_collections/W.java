0649b611c00c437e9dd2485c244aa1583c8f4923
Sahil dev barman <sahildevbarman369@gmail.com>
2026-09-12 19:59:28 +0530

    2026-09-12 | Collection - new example

---
+++ 40_collections/W.java
@@ -0,0 +1,41 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
+class W {
+    public static void main(String[] args) {
+        ArrayList<Employee> x = new ArrayList<Employee>();
+
+        x.add(new Employee("kartik", 20));
+        x.add(new Employee("raghav", 34));
+        x.add(new Employee("kejriwal", 56));
+        x.add(new Employee("rahul", 55));
+        x.add(new Employee("khadge", 74));
+
+        System.out.println(x);
+
+        Collections.sort(x);
+
+        System.out.println(x);
+    }
+}
+
+class Employee implements Comparable {
+    String name;
+    int age;
+
+    Employee(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String toString() {
+        return name + "#" + age;
+    }
+
+    public int compareTo(Object o) {
+        String nm1 = this.name;
+        String nm2 = ((Employee)o).name;
+
+        return nm2.compareTo(nm1);
+    }
+}
