c1c64bdcdeac9b3254f5239417f21adb8049ec93
Sahil dev barman <sahildevbarman369@gmail.com>
2026-09-16 18:52:18 +0530

    2026-09-16 | Collection - new example

---
+++ 40_collections/Y1.java
@@ -0,0 +1,24 @@
+import java.util.Collections;
+import java.util.ArrayList;
+
+class Y1 {
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<Integer>();
+
+        list.add(78);
+        list.add(18);
+        list.add(48);
+        list.add(67);
+        list.add(23);
+        list.add(59);
+
+        Collections.sort(list);
+
+        System.out.println(list);
+
+        // Integer x = Integer.valueOf(48);
+        // Integer x = Integer.valueOf(23);
+        Integer x = Integer.valueOf(78);
+        System.out.println(Collections.binarySearch(list, x));
+    }    
+}
